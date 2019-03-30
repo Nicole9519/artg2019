5d7d67639e144accfceaf708aee12c233e18a51e
Hanyang <35242674+Nicole9519@users.noreply.github.com>
2019-03-29 20:32:58 -0400

    Add files via upload

--- test/final/area.js
+++ test/final/area.js
@@ -5,11 +5,11 @@ Promise.all([dataPromise,districtPromise])
   .then(([housing, district]) => {
 
     const data = transform("08", housing);
-    console.log(housing)
+    console.log(data)
     render(data);
     
     const menu = d3.select(".nav")
-            .append("select");
+      .append("select");
 
     menu.selectAll("option")
       .data(district)
@@ -21,48 +21,46 @@ Promise.all([dataPromise,districtPromise])
     menu.on("change", function(){
 
       const code = this.value;
-
       const data = transform(code,housing);
-      
       render(data)
-
-
+    
     })
   //   console.log(housing)
   // drawHis2(d3.select('.module').node(), housing) 
   })
 
 function drawHis2(rootdom, data){
-const margin = {top: 10, right: 30, bottom: 30, left: 40},
-    width = 460 - margin.left - margin.right,
-    height = 400 - margin.top - margin.bottom;
+  
+  const margin = {top: 10, right: 30, bottom: 30, left: 40};
+  const width = 460 - margin.left - margin.right;
+  const height = 400 - margin.top - margin.bottom;
 
 
 
-  // X axis: scale and draw:
+    // X axis: scale and draw:
   const x = d3.scaleLinear()
-      .domain([0, 400])     // can use this instead of 1000 to have the max of data: d3.max(data, function(d) { return +d.price })
-      .range([0, width]);
+    .domain([0, 400])     // can use this instead of 1000 to have the max of data: d3.max(data, function(d) { return +d.price })
+    .range([0, width]);
 
   // set the parameters for the histogram
   const histogram = d3.histogram()
-      .value(function(d) { return d.square; })   // I need to give the vector of value
-      .domain(x.domain())  // then the domain of the graphic
-      .thresholds(x.ticks(70)); // then the numbers of bins
+    .value(function(d) { return d.square; })   // I need to give the vector of value
+    .domain(x.domain())  // then the domain of the graphic
+    .thresholds(x.ticks(70)); // then the numbers of bins
 
   // And apply this function to data to get the bins
   const bins = histogram(data);
 
   // Y axis: scale and draw:
   const y = d3.scaleLinear()
-      .range([height, 0]);
-      y.domain([0, d3.max(bins, function(d) { return d.length; })]);   // d3.hist has to be called before the Y axis obviously
+    .range([height, 0]);
+  y.domain([0, d3.max(bins, function(d) { return d.length; })]);   // d3.hist has to be called before the Y axis obviously
 
   // append the svg object to the body of the page
   const svg = d3.select(rootdom)
-              .classed("histogram",true)
-              .selectAll("svg")
-              .data([1])
+    .classed("histogram",true)
+    .selectAll("svg")
+    .data([1])
 
   const svgEnter = svg.enter()
     .append("svg")
@@ -70,45 +68,45 @@ const margin = {top: 10, right: 30, bottom: 30, left: 40},
   svg.merge(svgEnter)
     .attr("width", width + margin.left + margin.right)
     .attr("height", height + margin.top + margin.bottom)
-   
+     
   const plotEnter = svgEnter.append("g")
-      .attr("class","plot")
-      .attr("transform",
-          "translate(" + margin.left + "," + margin.top + ")");
+    .attr("class","plot")
+    .attr("transform",
+            "translate(" + margin.left + "," + margin.top + ")");
 
-      plotEnter.append("g")
-        .attr("class","axis-x")
-        .attr("transform", "translate(0," + height + ")")
+  plotEnter.append("g")
+    .attr("class","axis-x")
+    .attr("transform", "translate(0," + height + ")")
 
-      plotEnter.append("g")
-        .attr("class","axis-y")
+  plotEnter.append("g")
+    .attr("class","axis-y")
 
-      plotEnter
-        .append("rect") 
-        .attr("class","rect")
-        .style("fill", "#69b3a2")
-
+  plotEnter
+    .append("rect") 
+    .attr("class","rect")
+    .style("fill", "#ff5a5f")
 
 
   // append the bar rectangles to the svg element
   const plot = svg.merge(svgEnter).select(".plot")
  
   plot.selectAll(".rect")
-      .datum(bins)
-      .attr("x", 1)
-      .attr("transform", function(d) { return "translate(" + x(d.x0) + "," + y(d.length) + ")"; })
-      .attr("width", function(d) { return x(d.x1) - x(d.x0) -1 ; })
-      .attr("height", function(d) { return height - y(d.length); })
+    .data(bins)
+    .attr("x", 1)
+    .attr("transform", function(d) { return "translate(" + x(d.x0) + "," + y(d.length) + ")"; })
+    .attr("width", function(d) { return x(d.x1) - x(d.x0) - 1 ; })
+    .attr("height", function(d) { return height - y(d.length); })
   plot.select(".axis-x")
-      .call(d3.axisBottom(x));
+    .call(d3.axisBottom(x));
   plot.select(".axis-y")
-      .call(d3.axisLeft(y));
+    .call(d3.axisLeft(y));
 
 
 
 }
 
 function transform(district, data){
+  
   const filterData = data.filter(d => d.district === district );
 
   return filterData;
@@ -141,10 +139,10 @@ function parseData(d){
 
 function parseDistrictData(d){
   return {
-     name: d["Name"],
-     code: d["Division code[1]"].slice(-2),
-     area: d["Area (km_)"],
-     population: d["Population (2017_"]
+    name: d["Name"],
+    code: d["Division code[1]"].slice(-2),
+    area: d["Area (km_)"],
+    population: d["Population (2017_"]
   }
 }
 
